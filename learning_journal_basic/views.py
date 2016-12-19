1491d8967330ba6a4f051664eac13d3be9d702a3
Marc Fieser <midfies@gmail.com>
2016-12-19 15:44:01 -0800

    added mockups

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -1,6 +1,14 @@
-from pyramid.view import view_config
+from pyramid.response import Response
+import os
+import io
+
+THIS_DIR = os.path.dirname(__file__)
+
+def home_page(request):
+    file_path = os.path.join(THIS_DIR, "data", "sample.html")
+    file_data = io.open(file_path).read()
+    return Response(file_data)
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project': 'learning_journal_basic'}
+def includeme(config):
+    config.add_view(home_page, route_name="home")
