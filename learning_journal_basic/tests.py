f7aee3b253f087ba3a97843c407161c3c7abc262
Marc Fieser <midfies@gmail.com>
2016-12-21 00:23:06 -0800

    almost thought about testing

--- learning_journal_basic/tests.py
+++ learning_journal_basic/tests.py
@@ -18,12 +18,20 @@ def testapp():
     return TestApp(app)
 
 
-def test_home_page_renders_file_date(req):
+def test_home_page_renders_home_page_stuff(req):
     """My home page view returns some data."""
-    from .views import list_home
-    response = list_home(req)
+    from .views import list_view
+    response = list_view(req)
     assert "Most Recent Posts" in response
 
-def test_home_page_has_thing(testapp):
-    response = testapp.get("/", status=200)
-    html = response.html    
+# not removed cause i know i need this
+# def test_home_page_has_thing(testapp):
+#     response = testapp.get("/", status=200)
+#     html = response.html    
+
+
+def test_details_page_renders_details_page_stuff(req):
+    """My home page view returns some data."""
+    from .views import detail_view
+    response = detail_view(req)
+    assert "An individual Post" in response
